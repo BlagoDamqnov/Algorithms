b693652e6a932cfe66a0d5951db6fb0dcd07e30d
BlagoDamqnov <chessplayer89@abv.bg>
2022-02-06 14:30:38 +0200

    Algorithms

---
+++ Bubble Sort/Bubble Sort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BubbleSort(25, 17, 8, 5, 45, 32, 14, 12, 4, 1);
+        }
+
+       static void BubbleSort(params double [] arr)
+        {
+            double temp;
+
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                for (int j = 0; j < arr.Length-(i+1); j++)
+                {
+                    if (arr[j]>arr[j+1])
+                    {
+                        temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(", ",arr));
+        }
+    }
+}
---
+++ Insert Sort/Insert Sort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Insert_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertSort(25, 12, 7, 45, 10, 9, 112, 45, 25, 145, 132, 102);
+        }
+
+        static void InsertSort(params double [] arr)
+        {
+            int i = 1;
+            int j = i;
+            double temp = 0;
+
+            while (i<arr.Length)
+            {
+                j = i;
+                while (j>0&&arr[j-1]>arr[j])
+                {
+                    temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    j--;
+                }
+                i++;
+            }
+            Console.WriteLine(string.Join(", ",arr));
+        }
+    }
+}
